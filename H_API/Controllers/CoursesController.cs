855722afe858ba0dcddc122dd1277547676541ea
ghostEncoder <codeboyjedi@gmail.com>
2021-04-10 10:39:30 +0530

    ADDED CONTROLLERS

--- H_API/Controllers/CoursesController.cs
+++ H_API/Controllers/CoursesController.cs
@@ -4,11 +4,20 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using H_API.Models;
 
 namespace H_API.Controllers
 {
     public class CoursesController : ApiController
     {
+        //CREATE ENTITIY OBJECT
+        Online_examEntities db = new Online_examEntities();
 
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+           var courses =  db.Courses.ToList();
+            return Ok(courses);
+        }
     }
 }
