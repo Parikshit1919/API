57f3760ced9b5be7dfc642f8e9d7e716a891a773
ghostEncoder <codeboyjedi@gmail.com>
2021-04-10 10:30:13 +0530

    ADDED LOGIN AND REGISTRATION CONTROLLER

---
+++ H_API/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using H_API.Models;
+
+namespace H_API.Controllers
+{
+    public class LoginController : ApiController
+    {
+        //CREATE ENTITIY OBJECT
+        Online_examEntities db = new Online_examEntities();
+
+        //METHOD TO CHECK FOR STUDENT LOGIN CREDENTIALS
+        [HttpPost]
+        public IHttpActionResult Post(Student student)
+        {
+            //CHECK IF STUDENT RECORDS EXIST
+            var check = db.Students.Where(a => a.Student_Email == student.Student_Email && a.Student_Password == student.Student_Password).FirstOrDefault();
+            if(check!=null)
+            {
+                return Ok("valid");
+            }
+            else 
+            {
+                return Ok("invalid");
+            }
+        }
+    }
+}
---
+++ H_API/Controllers/RegistrationController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using H_API.Models;
+
+namespace H_API.Controllers
+{
+    public class RegistrationController : ApiController
+    {
+        //CREATE ENTITIY OBJECT
+        Online_examEntities db = new Online_examEntities();
+
+        //METHOD TO ADD NEW RECORDS INTO STUDENT TABLE
+        [HttpPost]
+        public IHttpActionResult Post([FromBody] Student student)
+        {
+            //CHECK IF STUDENT ALREADY REGISTERED
+            var check = db.Students.Where(a => a.Student_Email == student.Student_Email).FirstOrDefault();
+            //IF STUDENT NOT EXISTS ADD NEW RECORD
+            if(check==null)
+            {
+                db.Students.Add(student);
+                db.SaveChanges();
+                return Ok("added");
+            }
+            // ELSE RETURN ALREADY EXISTS
+            else 
+            {
+                return Ok("exists");
+            }
+
+        }
+    }
+}
